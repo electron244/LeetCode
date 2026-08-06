7e94e829ae7e0cbe64e12cc58386b9de8bb607c3
SIDHARTH SINH <160805750+electron244@users.noreply.github.com>
2026-08-06 22:04:15 +0530

    Time: 4 ms (80.97%), Space: 43.5 MB (30.65%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,24 +1,11 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] ori = heights.clone();
-        int[] expt = sortedArray(heights.clone());
+        int[] expected =heights.clone();
+        Arrays.sort(heights);
         int count = 0;
-        for(int i = 0 ;i<=heights.length-1;i++){
-            if(expt[i] != ori[i]) count++;
+        for(int i = 0;i<heights.length;i++){
+            if(expected[i] != heights[i]) count++;
         }
         return count;
-    }
-    public int[] sortedArray(int[] heights){
-        int n = heights.length;
-        for(int i = 0;i<n-1;i++){
-            int j = i+1;
-            while(j > 0 && heights[j-1] > heights[j]){
-                int temp = heights[j];
-                heights[j] = heights[j-1];
-                heights[j-1] = temp;
-                j--;
-            }
-        }
-        return heights;
     }
 }
